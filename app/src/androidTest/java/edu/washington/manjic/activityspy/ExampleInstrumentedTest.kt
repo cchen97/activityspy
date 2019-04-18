1187432191fc33e8a89063e1f45d76e1cbc227ac
cchen97 <manjic@uw.edu>
2019-04-17 20:51:14 -0700

    init

---
+++ app/src/androidTest/java/edu/washington/manjic/activityspy/ExampleInstrumentedTest.kt
@@ -0,0 +1,26 @@
+package edu.washington.manjic.activityspy
+
+import android.content.Context
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+
+        assertEquals("edu.washington.manjic.activityspy", appContext.packageName)
+    }
+}
---
+++ app/src/main/java/edu/washington/manjic/activityspy/MainActivity.kt
@@ -0,0 +1,53 @@
+package edu.washington.manjic.activityspy
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+//import android.R.attr.keySet
+
+class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.i(TAG, "onCreate event fired")
+        if (savedInstanceState != null) {
+            for (key in savedInstanceState.keySet()) {
+                Log.i(TAG, "Bundle:  " + key + " = \"" + savedInstanceState.get(key) + "\"")
+            }
+        }
+
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG, "onStart event fired")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "onResume event fired")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG, "onPause event fired")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG, "onStop event fired")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i(TAG, "onRestart event fired")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e(TAG, "We're going down, Captain!")
+    }
+}
